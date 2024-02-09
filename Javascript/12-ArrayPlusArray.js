ba6177842d9dd2494efbadb52879e846b9a043c7
Maan-py <mluqmaan22@gmail.com>
2024-02-09 10:19:23 +0700

    #12

--- Javascript/12-ArrayPlusArray.js
+++ Javascript/12-ArrayPlusArray.js
@@ -24,3 +24,13 @@
 const arrayPlusArray = (arr1, arr2) => [...arr1, ...arr2].reduce((acc, curr) => acc + curr);
 
 console.log(arrayPlusArray([100, 200, 300], [400, 500, 600]));
+
+// const capital = {
+//   Jakarta: "Indonesia",
+//   London: "England",
+//   Tokyo: "Japan",
+//   Indonesia : "Jakarta"
+// };
+// capital["New Delhi"] = "Indonesia";
+
+// console.log(capital["Indonesia"]);
