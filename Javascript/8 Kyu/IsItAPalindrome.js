366da882bdbc3914dac7ea93e86b048ef0ef07c3
Maan-py <mluqmaan22@gmail.com>
2024-04-06 16:32:53 +0700

    palindrome

--- Javascript/8 Kyu/IsItAPalindrome.js
+++ Javascript/8 Kyu/IsItAPalindrome.js
@@ -1,3 +1,14 @@
 function isPalindrome(x) {
   // your code here
+  if (x.length === 0 || x.length === 1) {
+    return true;
+  }
+
+ if(x[0] === x[x.length - 1]){
+   return isPalindrome(x.slice(1, x.length - 1))
+ }
+
+  return false;
 }
+
+console.log(isPalindrome("racecar"));
